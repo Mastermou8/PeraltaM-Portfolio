37b2441cce624f0dd3c69836d1b73199c4342622
Mauricio Peralta <mouperalta@gmail.com>
2026-01-09 16:39:24 -0500

    Add Mark My Campus project to gallery

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 
 export const Image = ({ title, largeImage, smallImage }) => {
+  const isExternalUrl = largeImage && largeImage.startsWith("http");
+
   return (
     <div className="portfolio-item">
       <div className="hover-bg">
-        {" "}
-        <a href={largeImage} title={title} data-lightbox-gallery="gallery1">
+        <a 
+          href={largeImage} 
+          title={title} 
+          data-lightbox-gallery="gallery1"
+          target={isExternalUrl ? "_blank" : "_self"}
+          rel={isExternalUrl ? "noopener noreferrer" : ""}
+        >
           <div className="hover-text">
             <h4>{title}</h4>
           </div>
-          <img src={smallImage} className="img-responsive" alt={title} />{" "}
-        </a>{" "}
+          <img src={smallImage} className="img-responsive" alt={title} />
+        </a>
       </div>
     </div>
   );
